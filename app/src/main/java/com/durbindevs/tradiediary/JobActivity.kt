392e54edc341a8b4a1111483605baa5f9a6aa1b6
Ivan Durbin <durbindevs@gmail.com>
2021-04-02 13:34:40 +1300

    Added layouts

--- app/src/main/java/com/durbindevs/tradiediary/JobActivity.kt
+++ app/src/main/java/com/durbindevs/tradiediary/JobActivity.kt
@@ -3,7 +3,7 @@ package com.durbindevs.tradiediary
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class JobActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
